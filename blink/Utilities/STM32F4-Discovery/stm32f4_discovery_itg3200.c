95d1b6d815d4cdd75ca532e7e11d533e0f1da02e
Anthony Paterson <arpaterson@live.com>
2013-10-14 20:42:24 +1300

    created templates for itg3200 support library, with doxygen comments.

---
+++ blink/Utilities/STM32F4-Discovery/stm32f4_discovery_itg3200.c
@@ -0,0 +1,96 @@
+/**
+  ******************************************************************************
+  * @file    stm32f4_discovery_itg3200.c
+  * @author  Anthony Paterson
+  * @version V0.0.0
+  * @date    14-OCtober-2013
+  * @brief
+  ******************************************************************************
+  * @attention
+  *
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
+#include "stm32f4_discovery_itg3200.h"
+
+ /** @addtogroup Utilities
+   * @{
+   */
+
+ /** @addtogroup STM32F4_DISCOVERY
+   * @{
+   */
+
+
+ /** @addtogroup STM32F4_DISCOVERY_ITG3200
+   * @{
+   */
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Private_TypeDefinitions
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Private_Defines
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Private_Macros
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Private_Variables
+   * @{
+   */
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Private_FunctionPrototypes
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Private_Functions
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+ /**
+   * @}
+   */
+
+ /**
+   * @}
+   */
+
+ /**
+   * @}
+   */
+
+ /**
+   * @}
+   */
+
+ /**
+   * @}
+   */
---
+++ blink/Utilities/STM32F4-Discovery/stm32f4_discovery_itg3200.h
@@ -0,0 +1,87 @@
+/**
+  ******************************************************************************
+  * @file    stm32f4_discovery_itg3200.h
+  * @author  Anthony Paterson
+  * @version V0.0.0
+  * @date    14-OCtober-2013
+  * @brief
+  ******************************************************************************
+  * @attention
+  *
+  *>
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef STM32F4_DISCOVERY_ITG3200_H_
+#define STM32F4_DISCOVERY_ITG3200_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+ /** @addtogroup Utilities
+   * @{
+   */
+
+ /** @addtogroup STM32F4_DISCOVERY
+   * @{
+   */
+
+
+ /** @addtogroup STM32F4_DISCOVERY_ITG3200
+   * @{
+   */
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Exported_Types
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Exported_Constants
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Exported_Macros
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+ /** @defgroup STM32F4_DISCOVERY_ITG3200_Exported_Functions
+   * @{
+   */
+
+ /**
+   * @}
+   */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* STM32F4_DISCOVERY_ITG3200_H_ */
+
+ /**
+   * @}
+   */
+
+ /**
+   * @}
+   */
+
+ /**
+   * @}
+   */
+
