8f585072b183155f47c19a2e03b4be5372f05633
Anthony Paterson <arpaterson@live.com>
2013-10-15 09:35:43 +1300

    new branch for seperating i2c support functions from ADXL345 files. added i2c.c/h

---
+++ blink/Utilities/STM32F4-Discovery/i2c.c
@@ -0,0 +1,8 @@
+/*
+ * i2c.c
+ *
+ *  Created on: 15/10/2013
+ *      Author: clarp0
+ */
+
+
---
+++ blink/Utilities/STM32F4-Discovery/i2c.h
@@ -0,0 +1,13 @@
+/*
+ * i2c.h
+ *
+ *  Created on: 15/10/2013
+ *      Author: clarp0
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+#endif /* I2C_H_ */
